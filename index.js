71fe0407ecea901f42b95a79b4fa77122b537a88
Joshua Taylor Eppinette <jteppinette@jteppinette.com>
2020-10-22 21:39:39 -0600

    Add cool faces endpoint

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
+
 const PORT = process.env.PORT || 5000
 
 express()
@@ -7,4 +9,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
