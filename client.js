54ef0a0734f67ec40e1556b3a5d9136843d8a889
Joshua Taylor Eppinette <jteppinette@jteppinette.com>
2020-10-23 00:24:39 -0600

    Add react

--- client.js
+++ client.js
@@ -1,2 +1,6 @@
-let element = document.getElementById('app')
-element.innerHTML = '<h1>Hello, World!</h1>'
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const App = () => <h1>Hello!</h1>
+
+ReactDom.render(<App />, document.getElementById('app'))
