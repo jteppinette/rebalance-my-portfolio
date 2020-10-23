9495e321d6af15720a281c394b031c10a500085b
Joshua Taylor Eppinette <jteppinette@jteppinette.com>
2020-10-23 01:56:42 -0600

    Restructure application

--- server/index.js
+++ server/index.js
@@ -4,5 +4,5 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'dist')))
+  .use(express.static(path.join(__dirname, '../', 'dist')))
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
