11a1065308e21dd2d1a25df430a170b5f2b17a9d
Joshua Taylor Eppinette <jteppinette@jteppinette.com>
2020-10-25 23:43:13 -0600

    Implement basic input functionality

--- client/app.js
+++ client/app.js
@@ -1,14 +1,58 @@
 import React, { Component } from 'react'
 
-import Table from './table'
+import Form from './form'
 
 class App extends Component {
+  initial = [{ symbol: '', currentBalance: 0, targetAllocation: 0 }]
+  state = {
+    investments: this.initial
+  }
+
   render () {
+    const { investments } = this.state
+
+    const addInvestment = this.addInvestment.bind(this),
+      resetInvestments = this.resetInvestments.bind(this),
+      removeInvestment = this.removeInvestment.bind(this),
+      updateInvestment = this.updateInvestment.bind(this)
+
     return (
       <div className='container'>
-        <Table />
+        <Form
+          investments={investments}
+          removeInvestment={removeInvestment}
+          updateInvestment={updateInvestment}
+        />
+        <div>
+          <button onClick={addInvestment}>Add Investment</button>
+          <button id='resetInvestments' onClick={resetInvestments}>
+            Reset Investments
+          </button>
+        </div>
       </div>
     )
+  }
+
+  addInvestment () {
+    this.setState({
+      investments: [
+        ...this.state.investments,
+        { symbol: '', currentBalance: 0, targetAllocation: 0 }
+      ]
+    })
+  }
+  resetInvestments () {
+    this.setState({ investments: this.initial })
+  }
+  removeInvestment (index) {
+    this.setState({
+      investments: this.state.investments.filter((investment, i) => i !== index)
+    })
+  }
+  updateInvestment (index, field, value) {
+    let investments = this.state.investments
+    investments[index][field] = value
+    this.setState({ investments })
   }
 }
 
