b0a4f1d0d47c12032a82c22397cbabdb224402f4
Joshua Taylor Eppinette <jteppinette@jteppinette.com>
2020-10-27 22:41:28 -0600

    Consolidate functionality into single app.js file

--- client/app.js
+++ client/app.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 
-import Form from './form'
-
 class App extends Component {
   initial = [{ symbol: '', currentBalance: 0, targetAllocation: 0 }]
   state = {
@@ -12,22 +10,73 @@ class App extends Component {
     const { investments } = this.state
 
     const addInvestment = this.addInvestment.bind(this),
-      resetInvestments = this.resetInvestments.bind(this),
       removeInvestment = this.removeInvestment.bind(this),
-      updateInvestment = this.updateInvestment.bind(this)
+      updateInvestment = this.updateInvestment.bind(this),
+      handleSubmit = this.handleSubmit.bind(this),
+      handleChange = this.handleChange.bind(this)
 
     return (
       <div className='container'>
-        <Form
-          investments={investments}
-          removeInvestment={removeInvestment}
-          updateInvestment={updateInvestment}
-        />
+        <form onSubmit={handleSubmit}>
+          <table className='striped-table'>
+            <thead>
+              <tr>
+                <th>Symbol</th>
+                <th>Current Balance</th>
+                <th>Target Allocation</th>
+                <th>Rebalance</th>
+                <th className='text-center'>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {investments.map((investment, index) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      <input
+                        type='text'
+                        name={`symbol-${index}`}
+                        value={investment.symbol}
+                        id={`symbol-${index}`}
+                        onChange={handleChange}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type='number'
+                        name={`currentBalance-${index}`}
+                        value={investment.currentBalance}
+                        id={`currentBalance-${index}`}
+                        onChange={handleChange}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type='number'
+                        name={`targetAllocation-${index}`}
+                        value={investment.targetAllocation}
+                        id={`targetAllocation-${index}`}
+                        onChange={handleChange}
+                      />
+                    </td>
+                    <td></td>
+                    <td className='text-center'>
+                      <button
+                        type='button'
+                        onClick={() => removeInvestment(index)}
+                        disabled={investments.length <= 1}
+                      >
+                        -
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </form>
         <div>
           <button onClick={addInvestment}>Add Investment</button>
-          <button id='resetInvestments' onClick={resetInvestments}>
-            Reset Investments
-          </button>
         </div>
       </div>
     )
@@ -41,9 +90,6 @@ class App extends Component {
       ]
     })
   }
-  resetInvestments () {
-    this.setState({ investments: this.initial })
-  }
   removeInvestment (index) {
     this.setState({
       investments: this.state.investments.filter((investment, i) => i !== index)
@@ -53,6 +99,15 @@ class App extends Component {
     let investments = this.state.investments
     investments[index][field] = value
     this.setState({ investments })
+  }
+  handleChange (event) {
+    const { name, value } = event.target
+    const [field, index] = name.split('-')
+    this.updateInvestment(index, field, value)
+  }
+  handleSubmit (event) {
+    event.preventDefault()
+    event.stopPropagation()
   }
 }
 
