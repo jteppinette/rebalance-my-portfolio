0f03e330394eafd65e350dc05dd2a373f9229709
Joshua Taylor Eppinette <jteppinette@jteppinette.com>
2021-08-26 16:26:44 -0600

    investment: Refactor to function based component

--- investment.js
+++ investment.js
@@ -1,68 +1,61 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import Numeric from './numeric'
 import { Input, Button } from 'reactstrap'
 
-export default class Investment extends Component {
-  render () {
-    const {
-      index,
-      symbol,
-      balance,
-      target,
-      rebalance,
-      update,
-      remove,
-      isRemoveDisabled
-    } = this.props
-
-    return (
-      <tr>
-        <td>
-          <Input
-            name={`symbol-${index}`}
-            value={symbol}
-            onChange={event => update('symbol', event.target.value)}
-          />
-        </td>
-        <td>
-          <Numeric
-            type='text'
-            name={`balance-${index}`}
-            value={balance}
-            onChange={(event, value) => update('balance', value)}
-            predefined='dollar'
-            className='form-control'
-          />
-        </td>
-        <td>
-          <Numeric
-            type='text'
-            name={`target-${index}`}
-            value={target}
-            onChange={(event, value) => update('target', value)}
-            predefined='percentageUS2dec'
-            minimumValue={0}
-            maximumValue={100}
-            className='form-control'
-          />
-        </td>
-        <td>
-          <Numeric
-            type='text'
-            name={`rebalance-${index}`}
-            value={rebalance}
-            predefined='dollar'
-            readOnly={true}
-            className='form-control'
-          />
-        </td>
-        <td className='text-center align-middle'>
-          <Button onClick={remove} disabled={isRemoveDisabled} color='danger'>
-            <i className='fas fa-minus'></i>
-          </Button>
-        </td>
-      </tr>
-    )
-  }
+function Investment (props) {
+  return (
+    <tr>
+      <td>
+        <Input
+          name={`symbol-${props.index}`}
+          value={props.symbol}
+          onChange={event => props.update('symbol', event.target.value)}
+        />
+      </td>
+      <td>
+        <Numeric
+          type='text'
+          name={`balance-${props.index}`}
+          value={props.balance}
+          onChange={(event, value) => props.update('balance', value)}
+          predefined='dollar'
+          className='form-control'
+        />
+      </td>
+      <td>
+        <Numeric
+          type='text'
+          name={`target-${props.index}`}
+          value={props.target}
+          onChange={(event, value) => props.update('target', value)}
+          predefined='percentageUS2dec'
+          minimumValue={0}
+          maximumValue={100}
+          className='form-control'
+        />
+      </td>
+      <td>
+        <Numeric
+          type='text'
+          name={`rebalance-${props.index}`}
+          value={props.rebalance}
+          predefined='dollar'
+          readOnly={true}
+          className='form-control'
+        />
+      </td>
+      <td className='text-center align-middle'>
+        <Button
+          onClick={props.remove}
+          disabled={props.isRemoveDisabled}
+          color='danger'
+        >
+          <i className='fas fa-minus'></i>
+        </Button>
+      </td>
+    </tr>
+  )
 }
+
+export default Investment
